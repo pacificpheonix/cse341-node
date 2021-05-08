f50bfa188f6912dfefdc0dff2d4151bca8e4ef49
Reece <bro18061@byui.edu>
2021-05-08 02:46:26 -0600

    More heroku stuff

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const PORT = process.env.PORT || 3000 // So we can run on heroku || (OR) localhost:3000
+
 const app = express();
 
 app.set('view engine', 'ejs');
